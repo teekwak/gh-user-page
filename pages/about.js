ff4b0af553253bfd8b9eef1296cc656b9b2258b5
Thomas Kwak <teekwak@gmail.com>
2018-10-17 23:04:32 -0700

    Create other main pages and use navbar component

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -10,14 +9,14 @@ import NavBar from '../common/navbar';
 const styles = theme => ({});
 
 
-class HomePage extends React.Component {
+class AboutPage extends React.Component {
     render = () => (
         <div>
             <NavBar />
             <Card>
                 <CardContent>
                     <Typography>
-                        Header
+                        About
                     </Typography>
                 </CardContent>
             </Card>
@@ -25,5 +24,5 @@ class HomePage extends React.Component {
     )
 }
 
-export default withStyles(styles)(HomePage);
+export default withStyles(styles)(AboutPage);
 
