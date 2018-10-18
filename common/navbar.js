47c7757c02d2dd53a1d300c5601f67f2b2c2b68b
Thomas Kwak <teekwak@gmail.com>
2018-10-17 23:33:21 -0700

    Remove style higher order component because of hydration mismatch

--- common/navbar.js
+++ common/navbar.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Link from 'next/link'
 
 
-const styles = theme => ({
-    navBarLink: {
-        textDecoration: 'none',
-        color: 'black'
-    }
-});
+const NavBar = props => (
+    <Toolbar>
+        <Button size="small">
+            <Link prefetch href="/">
+                <a style={{textDecoration: 'none', color: 'black'}}>Home</a>
+            </Link>
+        </Button>
+        <Button size="small">
+            <Link prefetch href="/tutorials">
+                <a style={{textDecoration: 'none', color: 'black'}}>Tutorials</a>
+            </Link>
+        </Button>
+        <Button size="small">
+            <Link prefetch href="/blog">
+                <a style={{textDecoration: 'none', color: 'black'}}>Blog</a>
+            </Link>
+        </Button>
+        <Button size="small">
+            <Link prefetch href="about">
+                <a style={{textDecoration: 'none', color: 'black'}}>About</a>
+            </Link>
+        </Button>
+    </Toolbar>
+);
 
-
-class NavBar extends React.Component {
-    render = () => (
-        <Toolbar>
-            <Button size="small"><Link prefetch href="/"><a className={this.props.classes.navBarLink}>Home</a></Link></Button>
-            <Button size="small"><Link prefetch href="/tutorials"><a className={this.props.classes.navBarLink}>Tutorials</a></Link></Button>
-            <Button size="small"><Link prefetch href="/blog"><a className={this.props.classes.navBarLink}>Blog</a></Link></Button>
-            <Button size="small"><Link prefetch href="/about"><a className={this.props.classes.navBarLink}>About</a></Link></Button>
-        </Toolbar>
-    )
-}
-
-export default withStyles(styles)(NavBar);
+export default NavBar;
 
